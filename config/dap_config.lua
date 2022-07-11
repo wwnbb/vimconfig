9afa4616c05b59e4e2face2fa714f295cc80fc40
wwnbb <wwnbb1@gmail.com>
2022-07-11 23:06:27 +0300

    dap config

--- config/dap_config.lua
+++ config/dap_config.lua
@@ -6,16 +6,7 @@ dap.adapters.go = {
   args = {os.getenv('NVIM_CONFIG') .. '/dist/debugAdapter.js'};
 }
 
-dap.configurations.go = {
-  {
-    type = 'go';
-    name = 'Debug';
-    request = 'launch';
-    showLog = false;
-    program = "${file}";
-    dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
-  },
-}
+require('dap-go').setup()
 
 require("nvim-dap-virtual-text").setup()
 
