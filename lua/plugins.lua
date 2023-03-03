cd85f046da634ae602f7fc1f82adc8e22b24b48c
wwnbb <wwnbb1@gmail.com>
2023-03-03 16:38:42 +0100

    add plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -52,16 +52,19 @@ return require('packer').startup(function(use)
   use 'rbong/vim-flog'
   use { 'kkoomen/vim-doge', run = 'npm i --no-save && npm run build:binary:unix' }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'nvim-telescope/telescope-github.nvim'
+
 
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = {
       'tom-anders/telescope-vim-bookmarks.nvim',
       'nvim-telescope/telescope-dap.nvim',
       'nvim-telescope/telescope-github.nvim',
-      'sopa0/telescope-makefile'
+      'sopa0/telescope-makefile',
     },
     config = function()
       require('telescope').load_extension('fzf')
+      require('telescope').load_extension('vim_bookmarks')
     end
   }
 
