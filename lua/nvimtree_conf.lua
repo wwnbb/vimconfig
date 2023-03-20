e22d45f8300aadf28b2caa783efe7f776605b3ef
wwnbb <wwnbb1@gmail.com>
2023-03-21 00:54:12 +0100

    update

--- lua/nvimtree_conf.lua
+++ lua/nvimtree_conf.lua
@@ -9,17 +9,5 @@ require("nvim-tree").setup({
   },
   filters = {
     dotfiles = true,
-  },
-  float = {
-    enable = true,
-    quit_on_focus_loss = true,
-    open_win_config = {
-      relative = "editor",
-      border = "rounded",
-      width = 30,
-      height = 30,
-      row = 1,
-      col = 1,
-    },
-  },
+  }
 })
