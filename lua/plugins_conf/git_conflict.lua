068c4bb439fbf2313cb8faa8e7d2dcdd7061cda4
wwnbb <wwnbb1@gmail.com>
2023-12-11 21:13:47 +0400

    update

--- lua/plugins_conf/git_conflict.lua
+++ lua/plugins_conf/git_conflict.lua
@@ -1,8 +1,8 @@
 require('git-conflict').setup(
   {
-    default_commands = true,     -- disable commands created by this plugin
-    disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-    highlights = {               -- They must have background color, otherwise the default color will be used
+    default_commands = true,    -- disable commands created by this plugin
+    disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+    highlights = {              -- They must have background color, otherwise the default color will be used
       incoming = 'DiffText',
       current = 'DiffAdd',
     },
--- lua/plugins_conf/init.lua
+++ lua/plugins_conf/init.lua
@@ -5,7 +5,7 @@ M.dap = require 'plugins_conf.dap'
 M.dressing = require 'plugins_conf.dressing'
 M.telescope = require 'plugins_conf.telescope'
 M.treesitter = require 'plugins_conf.treesitter'
-M.get_conflict = require 'plugins_conf.git_conflict'
+M.git_conflict = require 'plugins_conf.git_conflict'
 M.nvimtree = require 'plugins_conf.nvimtree'
 M.nvimtree = require 'plugins_conf.neotest'
 
